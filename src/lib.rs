5d5f110d6fe9955cc9fa59ac80aee19f43e54a90
snylonue <snylonue@gmail.com>
2020-07-24 11:18:20 +0800

    lib: add some basic struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,23 @@
 pub mod website;
+
+use reqwest::Url;
+use reqwest::header::HeaderMap;
+
+pub enum Format {
+    Video,
+    Audio,
+    Text,
+    Image,
+}
+pub struct FinataData {
+    pub url: Url,
+    pub raw_url: Vec<(Url, Format)>,
+    pub header: HeaderMap,
+    pub title: Option<String>,
+}
+
+impl FinataData {
+    pub const fn new(url: Url, raw_url: Vec<(Url, Format)>, header: HeaderMap, title: Option<String>) -> Self {
+        Self { url, raw_url, header, title }
+    }
+}
