b201c078ec800f195eba0e7432b9904c40d0bb9a
snylonue <snylonue@gmail.com>
2020-12-19 19:30:28 +0800

    website: add bilibili unfinished

---
+++ src/website/bilibili.rs
@@ -0,0 +1,96 @@
+use crate::error as err;
+use crate::utils;
+use crate::Extract;
+use crate::{Error, Finata};
+use lazy_static::lazy_static;
+use reqwest::{header, Client};
+use serde_json::Value;
+use snafu::ResultExt;
+use url::Url;
+
+lazy_static! {
+    static ref HEADERS: header::HeaderMap = crate::hdmap! {
+        header::USER_AGENT => utils::UA.clone(),
+        header::REFERER => "https://www.bilibili.com",
+    };
+    // add ?bvid={} or ?aid={}
+    // add cid={}, qn={} and avid/bvid={}
+    static ref CID_API: Url = Url::parse("https://api.bilibili.com/x/player/pagelist").unwrap();
+    static ref VIDEO_API: Url = Url::parse("https://api.bilibili.com/x/player/playurl").unwrap();
+}
+
+#[derive(Debug)]
+pub enum Id {
+    Av(String),
+    Bv(String),
+}
+
+pub struct Bilibili {
+    client: Client,
+    id: Id,
+}
+
+impl Id {
+    pub fn new(id: &str) -> Self {
+        if id.starts_with("av") {
+            Self::Av(id.trim_start_matches("av").to_owned())
+        } else if id.starts_with("BV") {
+            Self::Bv(id.to_owned())
+        } else {
+            todo!()
+        }
+    }
+}
+
+impl Bilibili {
+    pub fn new(s: &str) -> Result<Self, Error> {
+        let url: Url = Url::parse(s)?;
+        let id = url
+            .path_segments()
+            .map(|mut it| it.next_back())
+            .flatten()
+            .ok_or(Error::InvalidUrl {
+                url: url.to_owned(),
+            })?
+            .to_owned();
+        Ok(Self::with_id(id))
+    }
+    pub fn with_id(id: String) -> Self {
+        Self::with_client(utils::CLIENT.clone(), id)
+    }
+    pub fn with_client(client: Client, id: String) -> Self {
+        Self {
+            client,
+            id: Id::new(&id),
+        }
+    }
+    pub async fn playlist_json(&self) -> Result<Value, Error> {
+        let url = match &self.id {
+            Id::Av(av) => {
+                let mut url = CID_API.clone();
+                url.set_query(Some(&format!("aid={}", av)));
+                url
+            }
+            Id::Bv(bv) => {
+                let mut url = CID_API.clone();
+                url.set_query(Some(&format!("bvid={}", bv)));
+                url
+            }
+        };
+        Ok(self
+            .client
+            .get(url.clone())
+            .send()
+            .await
+            .context(err::NetworkError { url: url.clone() })?
+            .json()
+            .await?)
+    }
+}
+
+#[async_trait::async_trait]
+impl Extract for Bilibili {
+    async fn extract(&mut self) -> Result<Box<dyn Iterator<Item = Result<Finata, Error>>>, Error> {
+        todo!()
+    }
+}
--- src/website/mod.rs
+++ src/website/mod.rs
@@ -1,1 +1,2 @@
+pub mod bilibili;
 pub mod pixiv;
